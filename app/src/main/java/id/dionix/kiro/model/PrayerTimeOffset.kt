c97a95acb3d1bf1173ab2ef6df2b4a637d769ac3
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-03 01:17:09 +0700

    Added missing `RObject` implementation

--- app/src/main/java/id/dionix/kiro/model/PrayerTimeOffset.kt
+++ app/src/main/java/id/dionix/kiro/model/PrayerTimeOffset.kt
@@ -1,9 +1,34 @@
 package id.dionix.kiro.model
 
+import com.codedillo.rttp.model.RObject
+import com.codedillo.rttp.model.Value
+
 data class PrayerTimeOffset(
     var fajr: Int,
     var dhuhr: Int,
     var asr: Int,
     var maghrib: Int,
     var isha: Int
-)
+): RObject() {
+
+    override val data: List<Value>
+        get() = listOf(Value(fajr), Value(dhuhr), Value(asr), Value(maghrib), Value(isha))
+
+    override fun assign(list: List<Value>) {
+        if (list.size != 5) {
+            isValid = false
+            return
+        }
+
+        if (!list[0].isNumber() || !list[1].isNumber() || !list[2].isNumber() || !list[3].isNumber() || !list[4].isNumber()) {
+            isValid = false
+            return
+        }
+
+        fajr = list[0].toInt()
+        dhuhr = list[1].toInt()
+        asr = list[2].toInt()
+        maghrib = list[3].toInt()
+        isha = list[4].toInt()
+    }
+}
