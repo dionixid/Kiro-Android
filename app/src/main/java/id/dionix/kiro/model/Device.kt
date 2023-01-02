7fd0f90f53a31ab1c2d5315d7517916b2cdaf943
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-03 00:44:45 +0700

    add version to Device data model members

--- app/src/main/java/id/dionix/kiro/model/Device.kt
+++ app/src/main/java/id/dionix/kiro/model/Device.kt
@@ -5,24 +5,27 @@ import com.codedillo.rttp.model.Value
 
 data class Device(
     var id: String = "",
-    var name: String = ""
+    var name: String = "",
+    var version: String = ""
 ) : RObject() {
 
     override val data: List<Value>
-        get() = listOf(Value(id), Value(id))
+        get() = listOf(Value(id), Value(name), Value(version))
 
     override fun assign(list: List<Value>) {
-        if (list.size != 2) {
+        if (list.size != 3) {
             isValid = false
             return
         }
 
-        if (!list[0].isString() || !list[1].isString()) {
+        if (!list[0].isString() || !list[1].isString() || !list[2].isString()) {
             isValid = false
             return
         }
 
         id = list[0].toString()
         name = list[1].toString()
+        version = list[2].toString()
     }
+
 }
