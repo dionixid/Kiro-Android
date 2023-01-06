70ac14a96be14282bdfbb1deec7cc1f143ba3827
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-06 11:43:28 +0700

    Remove name and durationSeconds field (moved to SurahProperties)

--- app/src/main/java/id/dionix/kiro/model/Surah.kt
+++ app/src/main/java/id/dionix/kiro/model/Surah.kt
@@ -1,7 +1,28 @@
 package id.dionix.kiro.model
 
+import com.codedillo.rttp.model.RObject
+import com.codedillo.rttp.model.Value
+
 data class Surah(
     var id: Int = 0,
-    var name: String = "",
-    var durationSeconds: Int = 0
-)
+    var volume: Int = 20
+) : RObject() {
+
+    override val data: List<Value>
+        get() = listOf(Value(id), Value(volume))
+
+    override fun assign(list: List<Value>) {
+        if (list.size != 2) {
+            isValid = false
+            return
+        }
+
+        if (!list[0].isNumber() || !list[1].isNumber()) {
+            isValid = false
+            return
+        }
+
+        id = list[0].toInt()
+        volume = list[1].toInt()
+    }
+}
