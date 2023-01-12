de4665361c8f2ab739104876e76ef8d6eabc7eb2
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-12 15:47:21 +0700

    Add `RecyclerView` to display all surah list instead of just one

--- app/src/main/java/id/dionix/kiro/adapter/ScheduleAdapter.kt
+++ app/src/main/java/id/dionix/kiro/adapter/ScheduleAdapter.kt
@@ -1,20 +1,16 @@
 package id.dionix.kiro.adapter
 
 import android.view.LayoutInflater
-import android.view.View
+import android.view.MotionEvent
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import id.dionix.kiro.R
+import com.google.android.flexbox.FlexboxLayoutManager
 import id.dionix.kiro.databinding.ItemScheduleBinding
 import id.dionix.kiro.model.Prayer
 import id.dionix.kiro.model.QiroGroup
 import id.dionix.kiro.utility.ContentResolver
-import id.dionix.kiro.utility.runMain
 import id.dionix.kiro.utility.scaleOnClick
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class ScheduleAdapter(
     onItemSelected: (prayerName: Prayer.Name, qiroGroup: QiroGroup) -> Unit = { _, _ -> }
@@ -23,28 +19,31 @@ class ScheduleAdapter(
     private val mOnItemSelected = onItemSelected
 
     private var mItems: List<QiroGroup> = listOf()
-    set(value) {
-        val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
-            override fun getOldListSize(): Int {
-                return field.size
-            }
+        set(value) {
+            val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+                override fun getOldListSize(): Int {
+                    return field.size
+                }
 
-            override fun getNewListSize(): Int {
-                return value.size
-            }
+                override fun getNewListSize(): Int {
+                    return value.size
+                }
 
-            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-                return field[oldItemPosition] == value[newItemPosition]
-            }
+                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                    return field[oldItemPosition] == value[newItemPosition]
+                }
 
-            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-                return field[oldItemPosition] == value[newItemPosition]
-            }
-        })
+                override fun areContentsTheSame(
+                    oldItemPosition: Int,
+                    newItemPosition: Int
+                ): Boolean {
+                    return field[oldItemPosition] == value[newItemPosition]
+                }
+            })
 
-        field = value
-        diffResult.dispatchUpdatesTo(this)
-    }
+            field = value
+            diffResult.dispatchUpdatesTo(this)
+        }
 
     var currentPrayerName: Prayer.Name = Prayer.Name.Fajr
         set(value) {
@@ -78,63 +77,31 @@ class ScheduleAdapter(
         private val mBinding: ItemScheduleBinding,
     ) : RecyclerView.ViewHolder(mBinding.root) {
 
+        private val mAdapter = ScheduleSurahAdapter()
+
         init {
             mBinding.root.scaleOnClick {
                 mOnItemSelected(currentPrayerName, qiroGroup.copy())
+            }
+
+            mBinding.recyclerView.apply {
+                setHasFixedSize(false)
+                layoutManager = FlexboxLayoutManager(context)
+                itemAnimator = null
+                adapter = mAdapter
             }
         }
 
         var qiroGroup: QiroGroup = QiroGroup()
             set(value) {
                 field = value
-                val qiro = value.getQiro(currentPrayerName)
 
                 mBinding.tvTitle.apply {
                     text = ContentResolver.getDayName(context, value.dayOfWeek)
                 }
 
-                mBinding.cvQiroDuration.apply {
-                    setCardBackgroundColor(
-                        context.getColor(
-                            if (qiro.durationMinutes > 0) {
-                                R.color.green
-                            } else {
-                                R.color.red
-                            }
-                        )
-                    )
-                }
-
-                mBinding.tvQiroDuration.apply {
-                    text = if (qiro.durationMinutes > 0) {
-                        context.getString(
-                            if (qiro.durationMinutes > 1) {
-                                R.string.minutes_format
-                            } else {
-                                R.string.minute_format
-                            },
-                            qiro.durationMinutes
-                        )
-                    } else {
-                        context.getString(R.string.inactive)
-                    }
-                }
-
-                mBinding.cvSurah.apply {
-                    visibility = if (qiro.durationMinutes > 0) View.VISIBLE else View.GONE
-                }
-
-                mBinding.tvSurah.apply {
-                    if (qiro.surahList.isNotEmpty()) {
-                        CoroutineScope(Dispatchers.IO).launch {
-                            val name = ContentResolver.getSurahProperties(qiro.surahList[0]).name
-                            runMain {
-                                text = name
-                            }
-                        }
-                    } else {
-                        text = context.getString(R.string.inactive)
-                    }
+                mBinding.recyclerView.apply {
+                    mAdapter.setQiro(context, value.getQiro(currentPrayerName))
                 }
             }
 
