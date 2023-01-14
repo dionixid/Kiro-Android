d48c1be82d51ef0c0d4a07fb0355fdaa3566d7e9
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-14 22:11:33 +0700

    Add ability to select multiple items at once

--- app/src/main/java/id/dionix/kiro/adapter/AudioPickerAdapter.kt
+++ app/src/main/java/id/dionix/kiro/adapter/AudioPickerAdapter.kt
@@ -1,21 +1,31 @@
 package id.dionix.kiro.adapter
 
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import id.dionix.kiro.R
 import id.dionix.kiro.databinding.ItemSearchPlaylistBinding
+import id.dionix.kiro.dialog.AudioPreviewDialog
+import id.dionix.kiro.model.SurahAudio
 import id.dionix.kiro.model.SurahProperties
 import id.dionix.kiro.utility.format
+import id.dionix.kiro.utility.scaleOnClick
 import id.dionix.kiro.utility.secondsToTime
 
 class AudioPickerAdapter(
-    onItemSelected: (surahProps: SurahProperties) -> Unit = {}
+    private val mSupportFragmentManager: FragmentManager,
+    onSelectedItemChanged: (surahPropsList: List<SurahProperties>) -> Unit = {}
 ) : RecyclerView.Adapter<AudioPickerAdapter.ViewHolder>() {
 
-    private val mOnItemSelected = onItemSelected
+    private val mOnItemSelected = onSelectedItemChanged
 
     private var mItems: List<SurahProperties> = listOf()
+    val selectedItems: MutableList<SurahProperties> = mutableListOf()
+
+    private var mIsOpenDialog = false
 
     fun setSurahList(surahList: List<SurahProperties>) {
         val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
@@ -40,7 +50,24 @@ class AudioPickerAdapter(
         diffResult.dispatchUpdatesTo(this)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AudioPickerAdapter.ViewHolder {
+    fun select(surahProps: SurahProperties) {
+        val idx = selectedItems.indexOfFirst { it.id == surahProps.id }
+        if (idx == -1) {
+            selectedItems.add(surahProps)
+        } else {
+            selectedItems[idx] = surahProps.copy()
+        }
+
+        val position = mItems.indexOfFirst { it.id == surahProps.id }
+        if (position != -1) {
+            notifyItemChanged(position, surahProps)
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AudioPickerAdapter.ViewHolder {
         return ViewHolder(
             ItemSearchPlaylistBinding.inflate(
                 LayoutInflater.from(parent.context),
@@ -63,8 +90,42 @@ class AudioPickerAdapter(
     ) : RecyclerView.ViewHolder(mBinding.root) {
 
         init {
-            mBinding.root.setOnClickListener {
-                mOnItemSelected(surah.copy())
+            mBinding.root.apply {
+                setOnClickListener {
+                    val idx = selectedItems.indexOfFirst { it.id == surah.id }
+                    this@ViewHolder.isSelected = if (idx == -1) {
+                        selectedItems.add(surah.copy())
+                        true
+                    } else {
+                        selectedItems.removeAt(idx)
+                        false
+                    }
+                    mOnItemSelected(selectedItems)
+                }
+            }
+
+            mBinding.cvPlay.apply {
+                scaleOnClick {
+                    if (!mIsOpenDialog) {
+                        mIsOpenDialog = true
+                        AudioPreviewDialog(
+                            surah.name,
+                            context.getString(R.string.choose),
+                            SurahAudio(
+                                surah.id,
+                                surah.volume,
+                                isPaused = false,
+                                isPlaying = false
+                            ),
+                            onApply = { audio ->
+                                select(surah.copy(volume = audio.volume))
+                            },
+                            onDismiss = {
+                                mIsOpenDialog = false
+                            }
+                        ).show(mSupportFragmentManager, "dialog_audio_preview")
+                    }
+                }
             }
         }
 
@@ -74,6 +135,26 @@ class AudioPickerAdapter(
                 mBinding.tvId.text = String.format("%03d", value.id)
                 mBinding.tvName.text = value.name
                 mBinding.tvDuration.text = value.durationSeconds.secondsToTime().format("HH:mm:ss")
+                isSelected = selectedItems.indexOfFirst { it.id == value.id } != -1
+            }
+
+        private var isSelected = false
+            set(value) {
+                field = value
+
+                mBinding.ivCheckbox.apply {
+                    imageTintList = if (value) {
+                        setImageResource(R.drawable.ic_round_check_box)
+                        ColorStateList.valueOf(
+                            context.getColor(R.color.secondary)
+                        )
+                    } else {
+                        setImageResource(R.drawable.ic_round_check_box_outline_blank)
+                        ColorStateList.valueOf(
+                            context.getColor(R.color.text_default)
+                        )
+                    }
+                }
             }
 
     }
